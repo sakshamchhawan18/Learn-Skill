9ff88b7486a14b9528fe15d94564cc8ba3036f81
SakshamChhawan <sakshamchhawan18@gmail.com>
2024-06-04 20:55:05 +0530

    Setup of index and live pages

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,8 @@
+"Use Client"
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>Work in progress</h1>
+    </main>
+  );
+}
