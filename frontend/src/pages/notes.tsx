b854eedf9e3721bfe5b0a2f895a31003cca3bc5d
Ankit-Raj27 <ankitrajk27@gmail.com>
2024-06-04 21:10:49 +0530

    components

--- frontend/src/pages/notes.tsx
+++ frontend/src/pages/notes.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
 
+declare module 'react' {
+    interface JSX {
+        // Add the missing interface 'JSX.IntrinsicElements'
+        // to resolve the issue with JSX element implicitly having type 'any'.
+        interface IntrinsicElements {
+            [elemName: string]: any;
+        }
+    }
+}
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>Work in progress from notes</h1>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+                        <h1>Work in progress from notes</h1>
+        </main>
+    );
 }
