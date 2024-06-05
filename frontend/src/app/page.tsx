d1dfab87206dfeadba14e51d7517b0fc0a9304b9
SakshamChhawan <sakshamchhawan18@gmail.com>
2024-06-05 14:55:43 +0530

    changes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,8 @@
-import { NavbarDemo } from "@/components/navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1>njonc</h1>
-        <NavbarDemo />
     </main>
   );
 }
