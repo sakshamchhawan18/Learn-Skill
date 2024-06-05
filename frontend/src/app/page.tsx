ed6c7ef4b12ffd29dd5e603fe4d030e7d95790dc
Ankit-Raj27 <ankitrajk27@gmail.com>
2024-06-05 14:51:30 +0530

    Navbar menu

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,10 @@
+import { NavbarDemo } from "@/components/navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1>njonc</h1>
+        <NavbarDemo />
     </main>
   );
 }
