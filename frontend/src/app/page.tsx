233f4679d5c6faf8f2fd01bb9a30b2002989beb7
SakshamChhawan <sakshamchhawan18@gmail.com>
2024-06-05 15:07:33 +0530

    footer added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import { NavbarDemo } from "@/components/navbar";
+import Footer  from "@/components/footer";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1>njonc</h1>
         <NavbarDemo /> Hi
+        <Footer />
     </main>
   );
 }
