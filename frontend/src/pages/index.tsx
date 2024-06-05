4c996a1b9f1a51c2151e0598fdd7c915e9c08fab
Ankit-Raj27 <ankitrajk27@gmail.com>
2024-06-05 11:35:43 +0530

    navbar

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,12 @@
 "Use Client"
+
+import { NavbarDemo } from "@/components/navbar";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1>Work in progress</h1>
+            <NavbarDemo />
     </main>
   );
 }
