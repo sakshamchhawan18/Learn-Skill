c76119968554b71c8c5a2813fc70d5eab9516682
SakshamChhawan <sakshamchhawan18@gmail.com>
2024-06-05 21:15:03 +0530

    hero-section update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,12 @@
 import { NavbarDemo } from "@/components/navbar";
 import Footer  from "@/components/footer";
+import Herosection from "@/components/herosection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>njonc</h1>
-        <NavbarDemo /> Hi
+        <NavbarDemo />
+        <Herosection />
         <Footer />
     </main>
   );
---
+++ frontend/src/components/herosection.tsx
@@ -0,0 +1,21 @@
+"use client "
+
+import React from 'react'
+
+function herosection() {
+  return (
+    <div className='h-1/2 w-full bg-gray-50'>
+        <h1 className=" text-black '">herosection</h1>
+        <h2>
+            <p>
+            Welcome to Learn Skills, your one-stop platform for mastering in-demand skills and igniting your personal and professional growth! We offer a comprehensive learning experience with a variety of engaging formats to cater to your learning style.
+
+            </p>
+        </h2>
+
+        
+    </div>
+  )
+}
+
+export default herosection
