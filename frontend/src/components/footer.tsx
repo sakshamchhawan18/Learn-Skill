81ab66c31a90f20476fda6fe30bc3c0aaafcc659
SakshamChhawan <sakshamchhawan18@gmail.com>
2024-06-05 14:30:55 +0530

    restructure folder

---
+++ frontend/src/components/footer.tsx
@@ -0,0 +1,14 @@
+"Use Client"
+
+
+function footer() {
+   
+  return (
+    <div className="h-3 w-full border-zinc-950">
+        <h5 className="">copywrite 2025 @ Saksham & Ankit</h5>        
+    </div>
+        
+  )
+}
+
+export default footer
