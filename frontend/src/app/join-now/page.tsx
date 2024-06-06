c841999a5301d929aede8889faa90ae3c898dbd2
Ankit-Raj27 <ankitrajk27@gmail.com>
2024-06-06 16:20:35 +0530

    login page

--- frontend/src/app/join-now/page.tsx
+++ frontend/src/app/join-now/page.tsx
@@ -1,13 +1,22 @@
+"use client"
 import RoleSelection from "@/components/role-selection";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  
+  const handleClick = () => {
+    router.push('/login');
+  };
   return (
     <main className="flex flex-col items-center justify-between">
       <h1 className="text-4xl m-16 ">Join as a student or a teacher</h1>
       <RoleSelection />
-      Already have an account?<button className=" underline text-blue-400 ">Login</button>
-      {/* <RoleSelection /> */}
+      <p className="text-md mb-3 pt-5 mt-10  ">Already have an account?</p>
+      <button className=" underline text-md font-semibold text-gray-900 cursor-pointer w-40 p-2 bg-white shadow-md rounded-lg text-center hover:shadow-lg transition duration-300"  onClick={handleClick}>
+        Login
+      </button>
     </main>
   );
 }
