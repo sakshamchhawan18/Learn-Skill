2791d3f7e2b063322a385410acb86ab5006caa33
SakshamChhawan <sakshamchhawan18@gmail.com>
2024-06-06 12:57:43 +0530

    landing page updated

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,7 @@ import Footer  from "@/components/footer";
 import Herosection from "@/components/herosection";
 import { WobbleCard } from "@/ui/wobble-card";
 import { WobbleCardDemo } from "@/components/card";
+import { Coursesb } from "@/components/coursesb";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <NavbarDemo />
         <Herosection />
         <WobbleCardDemo />
+      <Coursesb />
         <Footer />
     </main>
   );
