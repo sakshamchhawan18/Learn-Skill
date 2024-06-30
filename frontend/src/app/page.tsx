a3843bb9d78c00b6aa9c1396b239cb6aab50ab74
Ankit-Raj27 <ankitrajk27@gmail.com>
2024-06-30 19:30:43 +0530

    [added signIn and signUp functionality]

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,21 +1,31 @@
+"use client"
 import { NavbarDemo } from "@/components/navbar";
-import Footer  from "@/components/footer";
+import Footer from "@/components/footer";
 import Herosection from "@/components/herosection";
 import { WobbleCardDemo } from "@/components/card";
 import { Coursesb } from "@/components/coursesb";
 import { Text } from "@/components/text";
+import { useRouter } from "next/navigation";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  
+  const router = useRouter();
+  const [user, loading, error] = useAuthState(auth);
+  const [pageLoading, setPageLoading] = useState(true);
+  useEffect(() => {
+    if (user) router.push("/dashboard");
+    if (!loading && !user) setPageLoading(false);
+  }, [user, router, loading]);
+  if(pageLoading) return null;
   return (
-    
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <NavbarDemo />
-        <Herosection />
-        <WobbleCardDemo />
-        <Text />
-        <Footer />
-        
+      <NavbarDemo />
+      <Herosection />
+      <WobbleCardDemo />
+      <Text />
+      <Footer />
     </main>
   );
 }
--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { cn } from "@/utils/cn";
@@ -8,32 +8,66 @@ import {
   IconBrandGoogle,
   IconBrandOnlyfans,
 } from "@tabler/icons-react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
+import { useRouter } from "next/navigation";
 
 export function Login() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Form submitted");
+  const [inputs, setInputs] = useState({ email: "", password: "" });
+
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs((prev) => ({ ...prev, [e.target.id]: e.target.value }));
   };
+  const router = useRouter();
+  const handleLogin = async (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!inputs.email || !inputs.password) {return alert("Please fill all fields!")};
+      try {
+        const newUser = await signInWithEmailAndPassword(   
+          inputs.email,
+          inputs.password
+          );
+        if (!newUser) {return};
+        router.push("/");
+      } catch (error: any) {
+        // toast.error("Login error, check details!", {position : "top-center", autoClose:2000, theme:"dark"})
+        alert(error.message);
+      }
+  };
+  if (error) alert(error.message);
+  useEffect(() => {}, [error]);
   return (
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
       <h1 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
         Welcome back to learn skill!
       </h1>
 
-      <form className="my-8" onSubmit={handleSubmit}>
+      <form className="my-8" onSubmit={handleLogin}>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+          <Input
+            onChange={handleInputChange}
+            id="email"
+            placeholder="projectmayhem@fc.com"
+            type="email"
+          />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="password">Password</Label>
-          <Input id="password" placeholder="••••••••" type="password" />
+          <Input
+            onChange={handleInputChange}
+            id="password"
+            placeholder="••••••••"
+            type="password"
+          />
         </LabelInputContainer>
 
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
-          
         >
           Login &rarr;
           <BottomGradient />
@@ -62,7 +96,6 @@ export function Login() {
             </span>
             <BottomGradient />
           </button>
-          
         </div>
       </form>
     </div>
