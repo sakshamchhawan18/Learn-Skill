7f37560c6a86012621284f07a6b8d87348a94302
Ankit-Raj27 <ankitrajk27@gmail.com>
2024-07-02 17:14:28 +0530

    [initialize database]

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -14,29 +14,37 @@ export function NavbarDemo() {
 function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
-    <div className="flex justify-center absolute -top-16 left-10  ">
-      <div className="flex w-full items-center justify-between m-0 p-0  ">
-        <div className="lg:w top-0">
-          <a
-            rel="noopener noreferrer"
-            href="#"
-            className="flex justify-center space-x-3 lg:justify-start"
-          >
-            <div className="flex items-center justify-center w-12 h-12 rounded-full dark:bg-violet-600">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 32 32"
-                fill="currentColor"
-                className="flex-shrink-0 w-5 h-5 rounded-full dark:text-gray-50"
-              >
-                <path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
-              </svg>
-            </div>
-            <span className="self-center text-2xl font-semibold">
-              Learn Skill.
-            </span>
-          </a>
+    <div className="flex justify-center absolute -top-16 left-10 right-10">
+      <div className="flex w-full items-center justify-between m-0 p-0">
+        <div className="flex w-full items-center justify-between m-0 p-0">
+          <div className="lg:w top-0">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="flex justify-center space-x-3 lg:justify-start"
+            >
+              <div className="flex items-center justify-center w-12 h-12 rounded-full dark:bg-violet-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 32 32"
+                  fill="currentColor"
+                  className="flex-shrink-0 w-5 h-5 rounded-full dark:text-gray-50"
+                >
+                  <path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
+                </svg>
+              </div>
+              <span className="self-center text-2xl font-semibold">
+                Learn Skill.
+              </span>
+            </a>
+          </div>
+          {/* <button className=" bg-red-500 text-white rounded mr-4">
+            SignUp
+          </button> */}
         </div>
+        {/* <div>
+          <button>SignIn</button>
+        </div> */}
         <div
           className={cn(
             "fixed top-10 inset-x-0 max-w-2xl mx-auto z-50",
@@ -45,7 +53,7 @@ function Navbar({ className }: { className?: string }) {
         >
           <Menu setActive={setActive}>
             <MenuItem setActive={setActive} active={active} item="Live">
-              <div className="  text-sm grid grid-cols-2 gap-10 p-4">
+              <div className="text-sm grid grid-cols-2 gap-10 p-4">
                 <ProductItem
                   title="Algochurn"
                   href="https://algochurn.com"
