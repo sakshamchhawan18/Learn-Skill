02204c05964203d7e3d9f4b7f34e559721d1aa5f
Ankit-Raj27 <ankitrajk27@gmail.com>
2024-08-01 04:39:31 +0530

    [student course page]

--- frontend/src/app/dashboard-student/Sidebar.tsx
+++ frontend/src/app/dashboard-student/Sidebar.tsx
@@ -52,7 +52,7 @@ export default function DashboardSideBar() {
               className={clsx("flex items-center gap-2 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50", {
                 "flex items-center gap-2 rounded-lg bg-gray-100 px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50": pathname === "/dashboard-student/kanban"
               })}
-              href="/dashboard-student/liveclasses"
+              href="/dashboard-student/courses"
             >
               <div className="border rounded-lg dark:bg-black dark:border-gray-800 border-gray-400 p-1 bg-white">
                 <FaTasks />
